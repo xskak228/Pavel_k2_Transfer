4046be99a6d1cb4de1b7525fbfe933c796ef0f98
xskak228 <m4xliom@yandex,ru>
2025-05-08 15:01:31 +0300

    Edit main page: 1. tariffs 2. reviews 3. footer Edit booking page: 1. form Edit sucses page: 1.Form ___ House, TPLV_PC

--- Pavel_k2_Vertical_site/Pavel_k2_Vertical_site/settings.py
+++ Pavel_k2_Vertical_site/Pavel_k2_Vertical_site/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    BASE_DIR / "static",
+    "/var/www/static/",
 ]
 
 # Default primary key field type
